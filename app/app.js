4b47a242eeaba2fb46f0119f3d9fde1b0341fd4c
dujar <dujar.coding@gmail.com>
2017-01-28 15:59:05 +0100

    before bootstrap

--- app/app.js
+++ app/app.js
@@ -3,8 +3,12 @@ var reload = require('reload');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
+app.locals.siteTitle = 'Nakhon Meetup!';
 app.use(require('./routes/index'));
+
 
 
 var server = app.listen(app.get('port'), function() {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res) {
-  res.send(`
-    <h1> Coffee shop in Nakhon Nayok</h1>
-    <p> but it does matter what you think</p>
-    <p> but it does matter what you think</p>
-    <p> but it does matter what you think</p>
-    <p> but it does matter what you think</p>
-    <script src="/reload/reload.js"></script>
-    `);
+
+  res.render('index', {
+    pageTitle: 'Home',
+    pageID: 'home',
+  });
+
 });
 
 module.exports = router;
