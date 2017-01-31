a006eda3d2f607dc6ed4d7538f13acf1417166ae
dujar <dujar.coding@gmail.com>
2017-01-31 23:30:27 +0100

    almost working, need to write the data to file from the form

--- app/app.js
+++ app/app.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var reload = require('reload');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var activities = require('./data/thingstodo.json');
+var io = require('socket.io')();
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('appData', activities);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.locals.siteTitle = 'Nakhon Meetup!';
+
+app.use(express.static('app/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./routes/index'));
-
-
 
 var server = app.listen(app.get('port'), function() {
   console.log('listen on port' + app.get('port'));
+
 });
 
 reload(server, app);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,44 @@
-
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res) {
+  var appData = req.app.get('appData');
+  appData.activity.forEach(function(activity) {
+    res.render('index', {
+      pageTitle: 'Home',
+      pageID: 'home',
+      activity: activity 
+    });
+  })
 
-  res.render('index', {
-    pageTitle: 'Home',
-    pageID: 'home',
+});
+
+router.post('/addactivity', function(req,res) {
+    var data = req.body;
+    var appData = req.app.get('appData');
+
+    console.log(data);
+    console.log(appData);
+    filePath = __dirname + '/data/thingstodo.json';
+    appData.activity += data;
+    console.log(appData.activity);
+
+    req.on('end', function (){
+        fs.appendFile(filePath, appData, function() {
+    res.end();
+        });
+  res.redirect('/activity');
+});
+
+});
+
+router.get('/activity', function(req,res) {
+  
+  res.render('activity', {
+  pageTitle: 'Add another Activity',
+  pageID: 'activity'
+
   });
-
 });
 
 module.exports = router;
